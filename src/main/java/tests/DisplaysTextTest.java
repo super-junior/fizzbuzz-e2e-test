1395da7094633b3ed0050aeddc0a57f9d32ad19f
Ika Belerma <ebelerma@thoughtworks.com>
2020-04-21 11:40:14 +0700

    [#20][Ika|Miew] Changed location of iOS .zip to releative path

--- src/main/java/tests/DisplaysTextTest.java
+++ src/main/java/tests/DisplaysTextTest.java
@@ -3,17 +3,20 @@ package tests;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-import pageobject.BaseScreen;
 import pageobject.FirstScreen;
 import pageobject.SecondScreen;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 public class DisplaysTextTest {
@@ -29,18 +32,26 @@ public class DisplaysTextTest {
         final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
         url = new URL(URL_STRING);
 
+        File app = getFilePath();
+
         capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11 Pro");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.4");
-        capabilities.setCapability(MobileCapabilityType.APP, "/tmp/application/FizzBuzz.app");
+        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
         capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-//        capabilities.setCapability("useNewWDA", false);
 
         driver = new IOSDriver<IOSElement>(url, capabilities);
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
+    }
+
+    private File getFilePath() {
+        File appDirectory = new File("./app");
+        Collection<File> apps = FileUtils.listFiles(appDirectory, new WildcardFileFilter("*.zip"), null);
+        System.out.println(apps);
+        return apps.iterator().next();
     }
 
     @AfterSuite
